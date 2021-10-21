1de116700531bb6dddf5561e074218685478fd4b
Zdeněk Škaryd <skaryd@obara.cz>
2021-10-21 10:45:53 +0200

    Ukol 1

---
+++ src/com/Úkol1/Promenne.java
@@ -0,0 +1,16 @@
+package com.Úkol1;
+
+import java.time.LocalDate;
+
+public class Promenne {
+    public static void main(String[] args) {
+        String jmenoKlienta;
+        int pocetNakupu;
+        String nazevMesta;
+        int spotreba;
+        int delakKabelu;
+        LocalDate datumNarozeni;
+        String spz;
+        int[]ipAddress=new int[4];
+    }
+}
--- src/com/Úkol1/Ukol1.java
+++
@@ -1,7 +0,0 @@
-package com.Úkol1;
-
-public class Ukol1 {
-    public static void main(String[] args) {
-        System.out.println("Ahoj");
-    }
-}
