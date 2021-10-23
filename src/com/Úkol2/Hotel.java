5c992c19675212edb7b3fe455c0df151d4ee0e97
Zdeněk Škaryd <skaryd@obara.cz>
2021-10-23 09:34:37 +0200

    23.10.21

--- src/com/Úkol2/Hotel.java
+++ src/com/Úkol2/Hotel.java
@@ -14,14 +14,15 @@ public class Hotel {
 //        System.out.println(jan.getDescription());
 
         Room room1=new Room(1,1,true,true,1000);
-//        Room room2=new Room(2,1,true,true,1000);
+        Room room2=new Room(2,1,true,true,1000);
         Room room3=new Room(3,3,false,true,2400);
 //        System.out.println(room1.getDescription());
 //        System.out.println(room2.getDescription());
 //        System.out.println(room3.getDescription());
 
-        Reservation res1=new Reservation(adela,null,room1,LocalDate.of(2021,7,19),LocalDate.of(2021,7,26));
-        Reservation res2=new Reservation(adela,jan,room3,LocalDate.of(2021,9,1),LocalDate.of(2021,9,14)); 
+        Reservation res1=new Reservation(adela,room1,LocalDate.of(2021,7,19),LocalDate.of(2021,7,26));
+        Reservation res2=new Reservation(adela,room3,LocalDate.of(2021,9,1),LocalDate.of(2021,9,14));
+        res2.setGuestList(jan);
         list.add(res1);
         list.add(res2);
 
@@ -31,6 +32,7 @@ public class Hotel {
             System.out.println("--------------------");
 
         }
+//        System.out.println(res2.getGuestList());
 
 
 
--- src/com/Úkol2/Reservation.java
+++ src/com/Úkol2/Reservation.java
@@ -1,41 +1,43 @@
 package com.Úkol2;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Reservation {
-    private Guest guest1;
-    private Guest guest2;
+    private Guest guest;
+    private ArrayList<Guest> guestList=new ArrayList<>();
     private Room room;
     private LocalDate comingDate;
     private LocalDate leavingDate;
 
-    public Reservation(Guest guest1, Guest guest2, Room room,LocalDate comingDate, LocalDate leavingDate) {
-        this.guest1 = guest1;
-        this.guest2 = guest2;
+    public Reservation(Guest guest, Room room,LocalDate comingDate, LocalDate leavingDate) {
+        this.guest = guest;
         this.room=room;
         this.comingDate = comingDate;
         this.leavingDate = leavingDate;
     }
 
    public String getDescription(){
-        if(guest2!=null)
-        return guest1.getDescription()+"\n"+guest2.getDescription()+"\n"+room.getDescription();
-        else
-        return guest1.getDescription()+"\n"+room.getDescription();
+         return guest.getDescription()+getGuestList()+"\n"+room.getDescription();
     }
 
-    public Guest getGuest1() {
-        return guest1;
-    }
-    public void setGuest1(Guest guest1) {
-        this.guest1 = guest1;
+    public String getGuestList(){
+        StringBuilder result = new StringBuilder();
+        for (Guest x:guestList) {
+            result.append("\n").append(x.getDescription());
+        }
+        return result.toString();
     }
 
-    public Guest getGuest2() {
-        return guest2;
+    public void setGuestList(Guest host) {
+        guestList.add(host);
     }
-    public void setGuest2(Guest guest2) {
-        this.guest2 = guest2;
+
+    public Guest getGuest() {
+        return guest;
+    }
+    public void setGuest(Guest guest) {
+        this.guest = guest;
     }
 
     public LocalDate getComingDate() {
